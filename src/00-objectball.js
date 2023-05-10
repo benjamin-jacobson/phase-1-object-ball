2ada19607e416e18736a88bbf13b5c5d894911d9
BenjaminJacobson <ben.jacobson@flatironschool.com>
2023-05-10 00:35:44 -0400

    completed lab

---
+++ src/00-objectball.js
@@ -0,0 +1,26 @@
+function gameObject(){
+    o = {
+        "home":{
+            "teamName":"Brooklyn Nets",
+            "colors":"Black,White",
+            "players": {
+                "Alan Anderson":{
+                    "number":0,
+                    "shoe":16,
+                    "points":22}
+                        }
+        },
+        "away":{
+            "teamName":"Charlotte Hornets",
+            "colors":"Turquoise, Purple",
+            "players": {
+                "Brendan Haywood":{
+                    "number":33,
+                    "shoe":25,
+                    "points":6
+            }
+            },
+        }
+    }
+    return o
+}
